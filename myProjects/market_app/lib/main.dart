d59da4fc295358a90d4b546f593b2d9e83300d53
kadzielas <sebastian.kadziela@outlook.com>
2024-08-26 11:57:44 +0200

    created new project - market_app

--- myProjects/market_app/lib/main.dart
+++ myProjects/market_app/lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:market_app/screens/home-screen.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_app/screens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(255, 200, 116, 48),
     brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
@@ -28,7 +27,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const TabsScreen(),
+      home: const HomeScreen(),
     );
   }
 }
